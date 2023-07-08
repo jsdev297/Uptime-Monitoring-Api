9f13decf3efc6adb04f43524c53f86113796ff02
Fatin Ishraq Prapya <prapyabakshi123@gmail.com>
2023-07-08 12:16:05 +0600

    cheking link of business logic done

--- Helpers/Environments.js
+++ Helpers/Environments.js
@@ -9,17 +9,18 @@ const environments = {};
 environments.staging = {
   port: 3000,
   name: "staging",
-  secretKey: "sfdsdfsdfsdfsdfsdf"
+  secretKey: "sfdsdfsdfsdfsdfsdf",
+  maxChecks: 5
 }
 
 environments.production = {
   port: 5000,
   name: "production",
-  secretKey: "sdfsdfsdfsdf"
+  secretKey: "sdfsdfsdfsdf",
+  maxChecks: 5
 }
 
 const currentEnvironment = typeof (process.env.NODE_ENV) === "string" ? process.env.NODE_ENV : "staging";
-
 const environmentToExport = typeof (environments[currentEnvironment]) === "object" ? environments[currentEnvironment] : "staging";
 
 module.exports = environmentToExport;
--- RouteHandler/CheckHandler.js
+++ RouteHandler/CheckHandler.js
@@ -6,8 +6,9 @@
 
 // Dependencies
 const data = require("../lib/data");
-const { hash, parseJSON } = require("../Helpers/Utilities");
+const { parseJSON, createRandomText } = require("../Helpers/Utilities");
 const tokenHandler = require("./TokenHandler");
+const { maxChecks } = require("../Helpers/Environments");
 
 // Module Scaffolding
 const checkHandler = {}
@@ -17,27 +18,95 @@ checkHandler.handle = (requestProperties, callback) => {
     const { method } = requestProperties;
     const acceptedMethods = ["get", "post", "put", "delete"];
     if (acceptedMethods.indexOf(method) > -1) {
-        userHandler._check[method](requestProperties, callback);
+        checkHandler._check[method](requestProperties, callback);
     } else {
         callback(405, { message: "" });
     }
 }
 
-userHandler._check = {};
+checkHandler._check = {};
 
-userHandler._check.post = (requestProperties, callback) => {
+checkHandler._check.post = (requestProperties, callback) => {
+    const protocol = typeof (requestProperties.body.protocol) === "string" && ["http", "https"].indexOf(requestProperties.body.protocol) > -1 ? requestProperties.body.protocol : false;
+    const url = typeof (requestProperties.body.url) === "string" && requestProperties.body.url.trim().length > 0 ? requestProperties.body.url : false;
+    const method = typeof (requestProperties.body.method) === "string" && ["GET", "POST", "PUT", "DELETE"].indexOf(requestProperties.body.method) > -1 ? requestProperties.body.method : false;
+    const successCodes = typeof (requestProperties.body.successCodes) === "object" && requestProperties.body.successCodes instanceof Array === true ? requestProperties.body.successCodes : false;
+    const timeOutSeconds = typeof (requestProperties.body.timeOutSeconds) === "number" && requestProperties.body.timeOutSeconds % 2 === 0 && requestProperties.body.timeOutSeconds >= 1 && requestProperties.body.timeOutSeconds <= 5 ? requestProperties.body.timeOutSeconds : false;
+
+
+    if (protocol && url && method && successCodes && timeOutSeconds) {
+        const token = typeof (requestProperties.headers.token) === "string" ? requestProperties.headers.token : false;
+        data.read("Tokens", token, (err, tokenData) => {
+            if (!err && tokenData) {
+                const userPhone = parseJSON(tokenData).phone;
+                data.read("users", userPhone, (err, userData) => {
+                    if (!err && userData) {
+                        tokenHandler._token.varifyToken(token, userPhone, (tokenId) => {
+                            if (tokenId) {
+                                const user = parseJSON(userData);
+                                const userChecks = typeof (user.checks) === "object" && user.checks instanceof Array ? user.checks : [];
+                                if (userChecks.length < maxChecks) {
+                                    const checkId = createRandomText(20);
+                                    const checkObject = { id: checkId, userPhone: user.phone, protocol, url, method, successCodes, timeOutSeconds: timeOutSeconds };
+                                    data.create("Checks", checkId, checkObject, (err) => {
+                                        if (!err) {
+                                            user.checks = userChecks;
+                                            user.checks.push(checkId);
+                                            data.update("users", user.phone, user, (err) => {
+                                                if (!err) {
+                                                    callback(200, checkObject);
+                                                } else {
+                                                    callback(500, {
+                                                        error: "There was a problem in the server 2"
+                                                    });
+                                                }
+                                            });
+                                        } else {
+                                            callback(500, {
+                                                error: "There was a problem in the server"
+                                            });
+                                        }
+                                    });
+                                } else {
+                                    callback(401, {
+                                        error: "User has already riched max checks link"
+                                    });
+                                }
+                            } else {
+                                callback(403, {
+                                    error: "Invalid Token"
+                                });
+                            }
+                        });
+                    } else {
+                        callback(403, {
+                            error: "User Not Found!"
+                        });
+                    }
+                });
+            } else {
+                callback(403, {
+                    error: "Authentication Problem"
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: "Problem in your input!"
+        });
+    }
 
 }
 
-userHandler._check.get = (requestProperties, callback) => {
+checkHandler._check.get = (requestProperties, callback) => {
 
 }
 
-userHandler._check.put = (requestProperties, callback) => {
+checkHandler._check.put = (requestProperties, callback) => {
 
 }
 
-userHandler._check.delete = (requestProperties, callback) => {
+checkHandler._check.delete = (requestProperties, callback) => {
 
 }
 
